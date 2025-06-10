8e7c195e846e8e2b225b71bf55b0ce4610124bff
Tamas Sallai <tamas.sallai.hu@gmail.com>
2025-06-10 12:47:09 +0200

    dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,18 @@ import icon from 'astro-icon'
 import sectionize from '@hbsnow/rehype-sectionize'
 
 import react from '@astrojs/react'
+import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon(), react()],
   site: 'https://example.com',
+
   output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
+
   i18n: {
     locales: ['hu', 'en'],
     defaultLocale: 'hu',
@@ -18,6 +24,7 @@ export default defineConfig({
       prefixDefaultLocale: false,
     },
   },
+
   markdown: {
     rehypePlugins: [sectionize],
   },
